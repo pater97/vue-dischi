8fa657a69752b473f97c59cd48d6bea2a7705cf6
pater97 <giuseppepater997@gmail.com>
2021-11-15 12:04:24 +0100

    header completato

---
+++ src/components/siteHeader.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="header">
+        <img src="../assets/Spotify_logo_without_text.svg" alt="">
+    </div>
+</template>
+
+<style lang="scss">
+.header{
+    background-color: #2E3A46;
+    img{
+        color: #1ED660;
+        width: 2.5rem;
+        margin:.5rem 1rem;
+    }
+}
+</style>
