097d90e55bcecd10bef08da75592381f16a37731
pater97 <giuseppepater997@gmail.com>
2021-11-15 12:26:21 +0100

    bug: npm install axios not found

---
+++ src/components/card.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <img :src="image" alt="">
+        <h2>
+            {{title}}
+        </h2>
+        <h5>
+            {{artist}}
+        </h5>
+        <h6>
+            {{year}}
+        </h6>
+    </div>
+</template>
+
+<script>
+export default {
+    props:{
+        Image: String,
+        title: String,
+        artist:String,
+        year:String
+    }
+}
+</script>
---
+++ src/components/siteMain.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="main_container">
+        <card/>
+    </div>
+</template>
+
+<script>
+import card from './card.vue'
+import Axios from 'Axios'
+export default {
+    components:{
+        card
+    },
+     mounted(){
+    Axios
+    .get('https://flynn.boolean.careers/exercises/api/array/music')
+    .then(r =>{
+      console.log(r);
+    }).catch(e=>{
+      console.log(e,'ops!');
+    })
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
